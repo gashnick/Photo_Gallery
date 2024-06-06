7597365a09719d21b0826fcb2f650ebd3356e566
gashnick <gashnicolas@gmail.com>
2024-06-06 10:12:26 +0200

    js changes

--- src/js/style.js
+++ src/js/style.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const galleryItems = document.querySelectorAll(".gallery-item");
+  const galleryItems = document.querySelectorAll(".gallery-images");
 
   galleryItems.forEach((item) => {
     item.addEventListener("mouseenter", () => {
